595d2a07fe39058ce5cc8b16fb0f6967a2de4100
Andrew Hamlin <aahamlin@gmail.com>
2020-06-13 07:53:22 -0400

    state tests

--- test/state_provider_spec.js
+++ test/state_provider_spec.js
@@ -8,9 +8,13 @@ describe('state_provider', function () {
         expect(state.totalCount).to.be.equal(0);
     });
 
-    it('#state records requests and counts', function () {
+    it('#state records requests', function () {
         state.requests[123] = { requestId: 123 };
         expect(state.requests).to.have.property('123').and.to.deep.equal({ requestId: 123 });
     })
 
+    it('#state records totalCount', function () {
+        state.totalCount += 1;
+        expect(state.totalCount).to.be.equal(1);
+    });
 });
